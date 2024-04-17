99c49ec94efa3eab1aef77cbba8b6744cfcea2bd
Dmytro Bobylov <dimas.od@gmail.com>
2024-04-17 17:17:47 -0400

    better fact server

--- facts.py
+++ facts.py
@@ -1,13 +1,10 @@
 import keyboard
 import requests
 
-API_KEY = ""
-LIMIT = 1
-
 
 def get_fact() -> dict | None:
-    api_url = f"https://api.api-ninjas.com/v1/facts?limit={LIMIT}"
-    response = requests.get(api_url, headers={"X-Api-Key": API_KEY})
+    api_url = "https://jokesrv.fermyon.app/facts"
+    response = requests.get(api_url)
     if response.status_code == requests.codes.ok:
         return response.json()
     else:
@@ -17,7 +14,7 @@ def get_fact() -> dict | None:
 def monitor_keyboard() -> None:
     while True:
         if keyboard.is_pressed("alt+t") or keyboard.is_pressed("alt+y"):
-            fact = get_fact()[0].get("fact")
+            fact = get_fact().get("content")
             if fact:
                 keyboard.write(fact)
                 keyboard.press("enter")
